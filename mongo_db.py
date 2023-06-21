23f043d3b8516dc7952b481c3a63f1b19ee0a8d4
judeakinwale <judeakinwale@gmail.com>
2023-06-21 08:43:27 +0100

    update: db connection for azure

--- mongo_db.py
+++ mongo_db.py
@@ -1,8 +1,9 @@
+import os
 from pymongo import MongoClient
 from config import mongo_settings
 
 # Connect to MongoDB
-client = MongoClient(mongo_settings.MONGO_URI)
+client = MongoClient(mongo_settings.MONGO_URI or os.environ['MONGO_URI'])
 
 # Access the database
 db = client["pyams"]
